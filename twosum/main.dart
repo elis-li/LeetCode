f7760de7ba75ece94e2022c597f97ade11dee35a
Your Name <you@example.com>
2024-05-29 16:33:48 +0300

    sum og two integers

--- twosum/main.dart
+++ twosum/main.dart
@@ -1,3 +1,18 @@
-void main(){
 
+
+List<int> twoSum(List<int> nums, int target) {
+    
+Map<int, int> map = {};
+for (int i = 0; i < nums.length; i++){
+  int num2 = target - nums[i];
+if (map.containsKey(num2)){
+  return[map[num2]!, i];
 }
+map[nums[i]] = i;
+  }
+  return [];
+  }
+    
+  void main(){
+    print(twoSum([2,7,11,15], 9));
+  }
