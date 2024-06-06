6cc50d5619cd3815751abba1cf85673ea9bdf820
elis-li <petrushina.2014@mail.ru>
2024-06-06 21:54:02 +0300

    Remove

---
+++ 26.Remove Duplicates from Sorted Array/main.dart
@@ -0,0 +1,12 @@
+  int removeDuplicates(List<int> nums) {
+ final originalNums =  nums.toSet();
+ final uniqueNums = originalNums.toList();
+ uniqueNums.sort();
+ for (int i = 0; i < uniqueNums.length; i++){
+    nums[i] = uniqueNums[i];
+ }
+ return uniqueNums.length;
+  }
+  void main(){
+    print(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
+  }
