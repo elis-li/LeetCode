209b39f9ddb6ca26f99e637490ce0e01fd721453
elis-li <petrushina.2014@mail.ru>
2024-06-20 19:20:08 +0300

    Index of the first occurence in a String

---
+++ 28.Find the Index of the First Occurence in a String/main.dart
@@ -0,0 +1,10 @@
+  int strStr(String haystack, String needle) {
+      if (!haystack.contains(needle)){
+        return (-1);
+      }
+      return haystack.indexOf(needle);
+  }
+
+  void main (){
+    print (strStr("sadbutsad", "sad"));
+  }
