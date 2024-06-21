de1d4b97d48f811553ea11ceea4bd055871b1f27
elis-li <petrushina.2014@mail.ru>
2024-06-21 17:12:16 +0500

    Converting

---
+++ 168.Excel Sheet Column Title/main.dart
@@ -0,0 +1,17 @@
+  String convertToTitle(int columnNumber) {
+
+    if (columnNumber < 27){
+      String letter = String.fromCharCode(64 + columnNumber);
+      return letter;
+    }
+    int first = columnNumber ~/ 26;
+    String firstLetter = String.fromCharCode(64 + first); 
+    int second = columnNumber % 26;
+    String secondLetter = String.fromCharCode(64 + second);
+    String title = firstLetter + secondLetter;
+    return title;
+  }
+
+  void main(){
+    print(convertToTitle(28));
+  }
