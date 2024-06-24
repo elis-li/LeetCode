19d8e5721833d041fef789af6267a745bdd7bb95
elis-li <petrushina.2014@mail.ru>
2024-06-24 21:23:37 +0500

    Contains Duplicates 217

--- 217.Contains Duplicates/main.dart
+++ 217.Contains Duplicates/main.dart
@@ -1,14 +1,15 @@
- bool containsDuplicate(List<int> nums) {
-    Set<int> original = {};
-Set<int> duplicates = {};
-for (int num in nums){
-  if (!original.add(num)){
-    (duplicates.add(num));
+bool containsDuplicate(List<int> nums) {
+  Set<int> original = {};
+  Set<int> duplicates = {};
+  for (int num in nums){
+    if (!original.add(num)){
+      (duplicates.add(num));
+    }
   }
-}
-if (duplicates.isEmpty) {
-  return false;
-} else return true;
+  if (duplicates.isEmpty) {
+    return false;
+  } else 
+  return true;
   }
 
   void main(){
