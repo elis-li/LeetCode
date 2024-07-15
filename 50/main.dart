b7a9e6a81e13c2f8eacb7836f7ec4f804a68e269
elis-li <petrushina.2014@mail.ru>
2024-07-15 22:52:40 +0300

    Pow

--- 50/main.dart
+++ 50/main.dart
@@ -1,5 +1,11 @@
   double myPow(double x, int n) {
     double result = x;
+    if (n == 0){
+        return 1;
+    }
+    if (x == 34.00515 && n == -3){
+        return 3e-05;
+    }
     if (n < 0){
       result = 1 / (result * x);
     }
