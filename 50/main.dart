a29bac459e2341155cc86d60aa849b52a2d30c73
elis-li <petrushina.2014@mail.ru>
2024-07-29 13:03:35 +0300

    Pow

--- 50/main.dart
+++ 50/main.dart
@@ -1,14 +1,21 @@
 double myPow(double x, int n) {
-    if (n == 0) return 1.0;
-    
-    double half = myPow(x, n ~/ 2);
-    
-    if (n % 2 == 0) {
-      return half * half;
-    } else {
-      return n > 0 ? half * half * x : half * half / x;
-    }
+  if (n == 0) return 1.0;
+  bool isNegativePower = n < 0;
+  if (isNegativePower) {
+    x = 1 / x;
+    n = -n;
   }
+  double result = 1.0;
+  double currentProduct = x;
+  while (n > 0) {
+    if (n % 2 == 1) {
+      result *= currentProduct;
+    }
+    currentProduct *= currentProduct;
+    n ~/= 2;
+  }
+  return result;
+}
 
   void main(){
     print(myPow(2.00000, 10));
